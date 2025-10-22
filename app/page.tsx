907b70a13d34a3015aa68b00372b5fe58539d87e
MeCaveman <o.alghamdi97@gmail.com>
2025-10-22 21:26:30 +0300

    fix5

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Translate from './translate'
+import Translate from '../translate'
 import Header from 'components/header'
 import Footer from 'components/footer'
 import Intro from 'components/intro'
 import Title from 'components/title'
 import Questions from 'components/questions'
-import ModalIndexPage from './modal'
+import ModalIndexPage from '../modal'
 
-export default function HomePage() {
+export default function EnglishPage() {
   return (
     <Translate>
       <div className='w-full'>
-        <Header /> {/* removed locale prop */}
+        <Header /> {/* locale prop removed */}
         <main role='main'>
           <div className='mx-auto max-w-7xl px-4 lg:px-8 pt-20 pb-16 text-slate-900 dark:text-slate-200'>
             <Intro />
