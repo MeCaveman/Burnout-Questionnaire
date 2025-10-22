ac58d5875a27f7a2d5dca89e540e7a3cfd64511f
MeCaveman <o.alghamdi97@gmail.com>
2025-10-22 21:48:25 +0300

    fix11

--- app/en/translate.tsx
+++ app/en/translate.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import translations from '../../lib/translate/locales/en.json'
-
+import translations from 'lib/translate/locales/en.json'
 
 import { ReactNode } from 'react'
-
-import { TranslateProvider } from '../../lib/contexts/translate'
+import { TranslateProvider } from 'lib/contexts/translate' // <-- This also needs to be an absolute path
 
 type Props = {
   children: ReactNode
